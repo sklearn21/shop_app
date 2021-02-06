d751356e7fcf3702f94992d8c8904f9c1332754f
Santosh Kawale <sklearn21@gmail.com>
2021-02-06 19:28:54 +0530

    Added Navigation to the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './screens/product_detail_screen.dart';
 import './screens/product_overview_screen.dart';
 
 void main() => runApp(MyApp());
@@ -15,6 +16,9 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Lato',
       ),
       home: ProductsOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+      },
     );
   }
 }
