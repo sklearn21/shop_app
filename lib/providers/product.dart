23bdb477d13e55d04897ec2ec3faf858ac8fe97b
Santosh Kawale <sklearn21@gmail.com>
2021-02-18 18:25:20 +0530

    Updating the Favourite status optmistically.

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import 'http_requests.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +22,26 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = baseUrl + 'products/$id.json';
+    try {
+      final response = await http.patch(url,
+          body: json.encode({
+            'isFavorite': isFavorite,
+          }));
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:shop_app/models/http_exception.dart';
 
+import '../models/http_exception.dart';
 import '../providers/http_requests.dart';
 import 'product.dart';
 
