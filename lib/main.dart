5872907e5beb287a86cd04aa3cdd88029c42abaa
Santosh Kawale <sklearn21@gmail.com>
2021-02-27 09:26:21 +0530

    Added proxy provider and token to http request.

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,17 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (ctx) => Auth()),
-          ChangeNotifierProvider(create: (ctx) => Products()),
+          ChangeNotifierProxyProvider<Auth, Products>(
+            update: (
+              ctx,
+              auth,
+              previousProducts,
+            ) =>
+                Products(
+              auth.token,
+              previousProducts == null ? [] : previousProducts.items,
+            ),
+          ),
           ChangeNotifierProvider(create: (ctx) => Cart()),
           ChangeNotifierProvider(create: (ctx) => Orders()),
         ],
